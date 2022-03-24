e1de8995ea33f01489271638feec3c1665bcc9c7
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-24 13:54:26 +0530

    Update productReducer.js

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,38 +1,43 @@
-export const productReducer=(state,action)=>{
-    switch (action.type) {
-        case "LOW_TO_HIGH":
-            return {...state,sortBy:action.type};
-         case "HIGH_TO_LOW":
-            return {...state,sortBy:action.type};
-        case "INCLUDE_JACKETS":
-            return {...state, includeJackets: !state.includeJackets }
-        case "INCLUDE_BAGS":
-            return {...state, includeBags: !state.includeBags }
-        case "INCLUDE_SHOES":
-            return {...state, includeShoes: !state.includeShoes }
-        case "INCLUDE_SUITCASE":
-            return {...state, includeSuitcase: !state.includeSuitcase }
-        case "RATING":
-                return { ...state, byRating: action.payload };
-        case "BRAND_ARNISA":
-            return {...state, brandArnisa: !state.brandArnisa }
-        case "BRAND_BIANYO":
-            return {...state, brandBianyo: !state.brandBianyo }
-        case "BRAND_CELINE":
-            return {...state, brandCeline: !state.brandCeline }
-        case "BRAND_GUCCI":
-            return {...state, brandGucci: !state.brandGucci }
-        case "RATING":
-                return { ...state, byRating: action.payload };
+export const productReducer = (state, action) => {
+  switch (action.type) {
+    case "LOW_TO_HIGH":
+      return { ...state, sortBy: action.type };
+    case "HIGH_TO_LOW":
+      return { ...state, sortBy: action.type };
+    case "INCLUDE_JACKETS":
+      return { ...state, includeJackets: !state.includeJackets };
+    case "INCLUDE_BAGS":
+      return { ...state, includeBags: !state.includeBags };
+    case "INCLUDE_SHOES":
+      return { ...state, includeShoes: !state.includeShoes };
+    case "INCLUDE_SUITCASE":
+      return { ...state, includeSuitcase: !state.includeSuitcase };
+    case "RATING":
+      return { ...state, byRating: action.payload };
+    case "BRAND_ARNISA":
+      return { ...state, brandArnisa: !state.brandArnisa };
+    case "BRAND_BIANYO":
+      return { ...state, brandBianyo: !state.brandBianyo };
+    case "BRAND_CELINE":
+      return { ...state, brandCeline: !state.brandCeline };
+    case "BRAND_GUCCI":
+      return { ...state, brandGucci: !state.brandGucci };
 
-        case "RESET":
-             return {
-                sortBy: "",
-    includeJackets : false, includeBags : false, includeShoes : false, includeSuitcase : false,
-    brandArnisa:false, brandGucci:false, brandCeline:false, brandBianyo:false,
-    priceRange: 1000,
-    byRating: ""};
-        default:
-            return state;
-    }
-}
+    case "RESET":
+      return {
+        sortBy: "",
+        includeJackets: false,
+        includeBags: false,
+        includeShoes: false,
+        includeSuitcase: false,
+        brandArnisa: false,
+        brandGucci: false,
+        brandCeline: false,
+        brandBianyo: false,
+        priceRange: 1000,
+        byRating: "",
+      };
+    default:
+      return state;
+  }
+};
