2094933b3be0a62e6e7d9fe7ea4faca99a42c8ea
Ranita Saha <saharanitaa1230@gmail.com>
2022-03-30 23:10:42 +0530

    update search

--- src/component/Navbar/Navbar.js
+++ src/component/Navbar/Navbar.js
@@ -1,60 +1,75 @@
-import React from 'react'
-import { Link, useLocation} from 'react-router-dom'
-import "./Navbar.css"
-import logo from "../../assets/images/logo.png"
-import { useData } from '../../contexts'
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../../assets/images/logo.png";
+import { useData } from "../../contexts";
 
 export function Navbar() {
   const {
-    cartState: { cart,wishList },dispatch
-  } = useData()
+    cartState: { cart, wishList },
+    dispatch,
+  } = useData();
   const { pathname } = useLocation();
-  
+
   return (
     <div>
       <header>
-      <div id="menu-bar" className="fas fa-bars menu-bar"></div>
-      <div className="logo-header">
-        <img src={logo} alt="logo" className="logo-design"/>
-        <a href="/" className="logo">Trekkart Store</a>
-      </div>
+        <div id="menu-bar" className="fas fa-bars menu-bar"></div>
+        <div className="logo-header">
+          <img src={logo} alt="logo" className="logo-design" />
+          <a href="/" className="logo">
+            Trekkart Store
+          </a>
+        </div>
 
-      <nav className="navbar">
-        <Link to="/" className="href">Home</Link>
-        <Link to="/products" className="href">Shop Now</Link>
-      </nav>
+        <nav className="navbar">
+          <Link to="/" className="href">
+            Home
+          </Link>
+          <Link to="/products" className="href">
+            Shop Now
+          </Link>
+        </nav>
 
-      {pathname ==="/products" && (
-        <form  className="class-form">
-        <input
-          type="search"
-          id="search-bar"
-          className="search-input"
-          placeholder="Search for products, brands and more..."
-          onChange={(e)=>dispatch({type:"FILTER_BY_SEARCH",payload:e.target.value})}
-        />
-        <label htmlFor="search-bar" className="fas fa-search search-label"></label>
-      </form>
-      )}
+        {pathname === "/products" && (
+          <form className="class-form">
+            <input
+              type="search"
+              id="search-bar"
+              className="search-input"
+              placeholder="Search for products, brands and more..."
+              onChange={(e) =>
+                dispatch({ type: "FILTER_BY_SEARCH", payload: e.target.value })
+              }
+            />
+            <label
+              htmlFor="search-bar"
+              className="fas fa-search search-label"
+            ></label>
+          </form>
+        )}
 
-      <div className="font-icons">
-        <Link to="/login">
-            <span className="badges-container"><i className="fas fa-user"></i>
+        <div className="font-icons">
+          <Link to="/login">
+            <span className="badges-container">
+              <i className="fas fa-user"></i>
             </span>
-        </Link>
+          </Link>
 
-        <Link to="/wishlist">
-        <span className="badges-container"><i className="fas fa-heart"></i>
-                <span className="badge-number">{wishList.length}</span>
+          <Link to="/wishlist">
+            <span className="badges-container">
+              <i className="fas fa-heart"></i>
+              <span className="badge-number">{wishList.length}</span>
             </span>
-        </Link>
-        <Link to="/cart">
-        <span className="badges-container"><i className="fas fa-shopping-cart"></i>
-                <span className="badge-number">{cart.length}</span>
+          </Link>
+          <Link to="/cart">
+            <span className="badges-container">
+              <i className="fas fa-shopping-cart"></i>
+              <span className="badge-number">{cart.length}</span>
             </span>
-        </Link>
-      </div>
-    </header>
+          </Link>
+        </div>
+      </header>
     </div>
-  )
+  );
 }
