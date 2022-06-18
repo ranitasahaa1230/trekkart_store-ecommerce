dce5170543fdc7e031fb59b7bd4c472652d7ae48
Ranita Saha <saharanitaa1230@gmail.com>
2022-06-18 17:53:02 +0530

    added Razorpay payment

--- src/pages/Cart/CartPrice.js
+++ src/pages/Cart/CartPrice.js
@@ -1,62 +1,118 @@
-import React from 'react'
-import { Link } from "react-router-dom";
-import { useData } from '../../contexts';
-import { getPriceDetails } from '../../utilities';
+import React from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useData } from "../../contexts";
+import { useToast } from "../../hooks";
+import { getPriceDetails } from "../../utilities";
 
 export function CartPrice() {
-    const {
-        cartState: { cart}} = useData();
-      const { newPrice, discount } = getPriceDetails(cart);
-      
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const {
+    cartState: { cart },
+  } = useData();
+  const { newPrice, discount } = getPriceDetails(cart);
+  const { showToast } = useToast();
+
+  const loadScript = (src) => {
+    return new Promise((resolve) => {
+      const script = document.createElement("script");
+      script.src = src;
+      script.onload = () => resolve(true);
+      script.onerror = () => resolve(false);
+      document.body.appendChild(script);
+    });
+  };
+
+  const displayRazorpay = async () => {
+    const response = await loadScript(
+      "https://checkout.razorpay.com/v1/checkout.js"
+    );
+
+    if (!response) {
+      showToast("error", "Razorpay SDK failed to load");
+    }
+
+    const options = {
+      key: "rzp_test_VSdp7X3K39GwBK",
+      amount: (newPrice-discount) * 100,
+      curreny: "INR",
+      name: "Trekkart Store",
+      description: "Thank you for shopping with us.",
+      image:
+        "https://therightfit.netlify.app/assets/The%20Right%20Fit-logos.jpeg",
+
+      handler: ({ payment_id }) => {
+        // cartDispatch({ type: "CLEAR_CART" });
+        navigate("/order");
+      },
+    };
+
+    const paymentObject = new window.Razorpay(options);
+    paymentObject.open();
+  };
+
   return (
-    <div className='section-price'>
-        <section className="section-cart">
-          <h3 className="heading-carts btm-b">Price Details</h3>
+    <div className="section-price">
+      <section className="section-cart">
+        <h3 className="heading-carts btm-b">Price Details</h3>
 
-          <div className="btm-b">
-            <div className="my-cart-sm">
-              <span>
-                Price <span>({cart.length} items)</span>
-              </span>
+        <div className="btm-b">
+          <div className="my-cart-sm">
+            <span>
+              Price <span>({cart.length} items)</span>
+            </span>
 
-              <span className="bold-txt">₹{newPrice}</span>
-            </div>
-
-            <div className="my-cart-sm">
-              <span>Discount</span>
-
-              <span className="txt-primary">- ₹{discount}</span>
-            </div>
-
-            <div className="my-cart-sm">
-              <span>Delivery Charges</span>
-
-              <span className="bold-txt">₹0</span>
-            </div>
-          </div>
-
-          <div className="btm-b">
-            <div className="my-cart-sm">
-              <span className="bold-txt bolder-txt">Total Amount</span>
-
-              <span className="bold-txt bolder-txt">₹Rs.{newPrice - discount}</span>
-            </div>
+            <span className="bold-txt">₹{newPrice}</span>
           </div>
 
           <div className="my-cart-sm">
-            <span className="txt-primary bolder-txt">
-              You will save ₹{discount} on this order
-            </span>
+            <span>Discount</span>
+
+            <span className="txt-primary">- ₹{discount}</span>
           </div>
 
-          <div>
+          <div className="my-cart-sm">
+            <span>Delivery Charges</span>
+
+            <span className="bold-txt">₹0</span>
+          </div>
+        </div>
+
+        <div className="btm-b">
+          <div className="my-cart-sm">
+            <span className="bold-txt bolder-txt">Total Amount</span>
+
+            <span className="bold-txt bolder-txt">
+              ₹Rs.{newPrice - discount}
+            </span>
+          </div>
+        </div>
+
+        <div className="my-cart-sm">
+          <span className="txt-primary bolder-txt">
+            You will save ₹{discount} on this order
+          </span>
+        </div>
+
+        <div>
+          {pathname === "/cart" && (
             <Link to="/address">
               <button className="btn btn-text-icon-primary grid-cards-icons details-icon">
                 PLACE ORDER
               </button>
             </Link>
-          </div>
-        </section>
+          )}
+
+          {pathname === "/checkout" && (
+            <button
+              className="btn btn-text-icon-primary grid-cards-icons details-icon"
+              onClick={displayRazorpay}
+            >
+              CHECKOUT
+            </button>
+          )}
+        </div>
+      </section>
     </div>
-  )
+  );
 }
--- src/pages/Checkout/Checkout.js
+++ src/pages/Checkout/Checkout.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 import { useNavigate } from "react-router-dom";
-import { useAddress } from '../../contexts';
+import { useAddress } from "../../contexts";
+import { CartPrice } from "../Cart/CartPrice";
 
 export const Checkout = () => {
-  const { newAddress, selected } = useAddress();
+  const { newAddress } = useAddress();
   const navigate = useNavigate();
 
   const finalAddress = newAddress?.filter(
@@ -11,13 +12,77 @@ export const Checkout = () => {
   );
 
   const handleChange = () => {
-    navigate("/address");
+    navigate(-1);
   };
 
   return (
     <div className="cart-section">
-          <h3 className="cart-heading">Confirm Order </h3>
+      <h3 className="cart-heading">Confirm Order </h3>
+      <div className="cart-grid address-form">
+          {finalAddress.map(
+            ({
+              _id,
+              name,
+              address,
+              mobile,
+              landmark,
+              country,
+              state,
+              zipCode,
+            }) => (
+              <div className="address-contain">
+                <div className="flex-address user-details">
+                  <input
+                    id={_id}
+                    type="radio"
+                    name="select"
+                    className="user-checkbox"
+                    checked
+                  />
+                  <span className="name-details">
+                    <b>Name:</b> {name}
+                  </span>
+                  <span className="name-details">
+                    <b>Phn No:</b> (+91){mobile}
+                  </span>
+                  <span>
+                    <b>Country:</b> {country}
+                  </span>
+                </div>
 
+                <div className="flex-address user-details">
+                  <span className="name-details">
+                    <b>State: </b>
+                    {state}
+                  </span>
+                  <span className="name-details">
+                    <b>Landmark:</b> {landmark}
+                  </span>
+                  <span>
+                    <b>ZipCode: </b>
+                    {zipCode}
+                  </span>
+                </div>
+
+                <div className="flex-address details">
+                  <div>
+                    <b>Address: </b>
+                    {address}
+                  </div>
+                </div>
+
+                <div className="update-btn-container" id="dummy-values">
+                  <button className="submit-loginbtns" onClick={handleChange}>
+                  <i className="fa-solid fa-backward"></i> {""}Go Back To Prev Page
+                  </button>
+                </div>
+              </div>
+            )
+          )}
+
+        <CartPrice />
     </div>
-  )
-}
+    </div>
+
+  );
+};
