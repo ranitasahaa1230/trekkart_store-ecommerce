96aa623ecfc48e3aee110f426d32ddd2c6537748
Ranita Saha <saharanitaa1230@gmail.com>
2022-06-18 21:45:15 +0530

    added react confetti

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,22 +1,27 @@
 import { useNavigate } from "react-router-dom";
+import Confetti from "react-confetti";
 import "./Order.css";
 
 export const Order = () => {
   const navigate = useNavigate();
-
   const handleConitnueShopping = () => {
     navigate("/products");
   };
 
   return (
-    <div className="order-container">
-      <h3 className="heading-2">Order Placed Successfully</h3>
-      <button
-        className="button"
-        onClick={handleConitnueShopping}
-      >
-        Continue Shopping
-      </button>
-    </div>
+    <>
+        <div className="cart-section">
+          <div className="order-container">
+            <h3 className="cart-head">Yayyy, Order Placed Successfully !🎉</h3>
+            <button
+              className="submit-loginbtn"
+              onClick={handleConitnueShopping}
+            >
+              Continue Shopping
+            </button>
+          </div>
+        </div>
+      <Confetti />
+    </>
   );
 };
