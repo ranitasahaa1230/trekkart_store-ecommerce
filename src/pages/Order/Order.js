d5b7ab5a4bd82dc1bb1cde5f246ab3b198cfe9d2
Ranita Saha <saharanitaa1230@gmail.com>
2022-06-18 21:24:53 +0530

    update order page

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useNavigate } from "react-router-dom";
+import "./Order.css";
 
 export const Order = () => {
+  const navigate = useNavigate();
+
+  const handleConitnueShopping = () => {
+    navigate("/products");
+  };
+
   return (
-    <div>Order</div>
-  )
-}
+    <div className="order-container">
+      <h3 className="heading-2">Order Placed Successfully</h3>
+      <button
+        className="button"
+        onClick={handleConitnueShopping}
+      >
+        Continue Shopping
+      </button>
+    </div>
+  );
+};
